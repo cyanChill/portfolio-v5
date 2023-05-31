0d5d1c785c631ba6d82bc64af7cf4e691ce0f1ab
cyanChill <anthonyliang9@gmail.com>
2023-05-31 13:51:24 -0400

    styling for contact button if on page

--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -12,7 +12,7 @@ import CenterLayout from "./CenterLayout";
 export default function NavBar() {
   const pathname = usePathname();
 
-  const [isActive, setIsActive] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   // Dynamically change the background based on the current route
   let bkgPattern =
@@ -27,10 +27,10 @@ export default function NavBar() {
 
   let navToggleClass =
     "z-50 fixed right-0 top-4 rounded-l-full p-1 px-4 text-3xl sm:hidden";
-  if (!isActive) navToggleClass += " gradient-primary bg-gradient-to-r";
+  if (!isOpen) navToggleClass += " gradient-primary bg-gradient-to-r";
 
   // Close nav on mobile when routes changes
-  useEffect(() => setIsActive(false), [pathname]);
+  useEffect(() => setIsOpen(false), [pathname]);
 
   return (
     <>
@@ -40,16 +40,16 @@ export default function NavBar() {
       <button
         className={navToggleClass}
         aria-controls="primary-navigation"
-        onClick={() => setIsActive((prev) => !prev)}
+        onClick={() => setIsOpen((prev) => !prev)}
       >
-        {!isActive ? <RiMenu4Line /> : <RiCloseLine />}
+        {!isOpen ? <RiMenu4Line /> : <RiCloseLine />}
         <span className="sr-only">Menu</span>
       </button>
 
       <CenterLayout>
         <nav
           className={`fixed left-0 top-0 z-40 flex h-screen w-screen flex-col overflow-y-auto bg-slate-950 px-12 sm:relative sm:mx-6 sm:mt-4 sm:h-auto sm:w-auto sm:flex-row sm:items-center sm:justify-between sm:bg-transparent sm:px-0 ${
-            isActive
+            isOpen
               ? "visible"
               : "pointer-events-none invisible sm:pointer-events-auto sm:visible"
           }`}
@@ -62,20 +62,20 @@ export default function NavBar() {
             className="absolute left-4 top-2 sm:static"
           />
 
-          <ul className="mt-auto flex flex-col gap-4 pt-32 text-3xl font-medium sm:absolute sm:left-1/2 sm:-translate-x-1/2 sm:flex-row sm:py-0 sm:text-lg">
+          <ul className="mt-auto flex flex-col gap-4 pt-32 text-3xl font-medium sm:absolute sm:left-1/2 sm:-translate-x-1/2 sm:flex-row sm:py-0 sm:text-lg md:gap-[5rem]">
             {["Home", "Projects", "About"].map((route) => {
               const derivedRoute =
                 route != "Home" ? `/${route.toLowerCase()}` : "/";
               const isActive = derivedRoute === pathname;
 
-              let linkClass =
-                "relative before:h-[2px] before:w-[1.5ch] before:bottom-0 before:left-0 before:absolute hocus:opacity-100 hocus:before:bg-white";
-              if (!isActive) linkClass += " opacity-70";
-              else linkClass += " before:bg-white";
-
               return (
                 <li key={route} className="w-fit">
-                  <Link href={derivedRoute} className={linkClass}>
+                  <Link
+                    href={derivedRoute}
+                    className={`relative before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-[1.5ch] hocus:opacity-100 hocus:before:bg-white ${
+                      isActive ? "before:bg-white" : "opacity-70"
+                    }`}
+                  >
                     {route}
                   </Link>
                 </li>
@@ -85,10 +85,16 @@ export default function NavBar() {
 
           <Link
             href="/contact"
-            className="mt-12 flex w-fit items-center gap-1 rounded-md bg-slate-800 p-2 py-1.5 font-bold sm:mt-0 sm:h-fit"
+            className={`mt-12 flex w-fit items-center gap-1 rounded-md border-2 p-2 py-1.5 font-bold sm:mt-0 sm:h-fit ${
+              pathname === "/contact"
+                ? "border-white"
+                : "border-slate-800 bg-slate-800"
+            }`}
           >
             {`Let's work together`}
-            <BsArrowRightShort />
+            <BsArrowRightShort
+              className={pathname === "/contact" ? "rotate-90" : ""}
+            />
           </Link>
 
           <div className="mt-8 flex gap-3 pb-32 text-2xl sm:hidden">
