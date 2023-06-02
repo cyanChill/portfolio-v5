4f1ebd4ca06858d68916e5b03e6adcdb54d0e605
cyanChill <anthonyliang9@gmail.com>
2023-06-02 19:18:07 -0400

    add error message under form inputs

--- app/contact/ContactForm.tsx
+++ app/contact/ContactForm.tsx
@@ -2,7 +2,7 @@
 
 import { useForm, UseFormRegister } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import type { Path, SubmitHandler } from "react-hook-form";
+import type { Path, SubmitHandler, FieldError } from "react-hook-form";
 import toast from "react-hot-toast";
 
 import { contactSchema, type ContactSchemaType } from "@/lib/schema";
@@ -38,9 +38,19 @@ export default function ContactForm() {
 
   return (
     <form onSubmit={handleSubmit(submitMessage)}>
-      <Input label="Name" register={register} />
-      <Input label="Email" type="email" register={register} />
-      <TextArea label="Message" register={register} rows={5} />
+      <Input label="Name" register={register} error={formState.errors.Name} />
+      <Input
+        label="Email"
+        type="email"
+        register={register}
+        error={formState.errors.Email}
+      />
+      <TextArea
+        label="Message"
+        register={register}
+        rows={5}
+        error={formState.errors.Message}
+      />
       <button
         type="submit"
         disabled={formState.isSubmitting}
@@ -55,46 +65,53 @@ export default function ContactForm() {
 type FormElement = {
   label: Path<ContactSchemaType>;
   register: UseFormRegister<ContactSchemaType>;
+  error?: FieldError;
 };
 
 type InputProps = { type?: string } & FormElement;
 
-const Input = ({ label, register, type = "text" }: InputProps) => {
+const Input = ({ label, register, error, type = "text" }: InputProps) => {
   return (
-    <div className="relative my-3 text-white opacity-70">
+    <div className="relative my-3 text-gray-400">
       <input
         type={type}
         placeholder=" "
         {...register(label)}
-        className="peer block w-full appearance-none rounded-md border-2 border-white bg-transparent px-2.5 pb-2.5 pt-4 text-sm focus:border-primary focus:outline-none focus:ring-0"
+        className="peer block w-full appearance-none rounded-md border-2 border-gray-400 bg-transparent px-2.5 pb-2.5 pt-4 text-sm focus:border-primary focus:outline-none focus:ring-0"
       />
       <label
         htmlFor={label}
-        className="pointer-events-none absolute left-1 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-slate-950 px-2 text-sm duration-300 peer-placeholder-shown:top-6 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:text-primary"
+        className="pointer-events-none absolute left-1 top-2 z-10 origin-[0] -translate-y-4 scale-90 transform bg-slate-950 px-2 text-sm duration-300 peer-placeholder-shown:top-6 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-90 peer-focus:text-fuchsia-400"
       >
         {label}
       </label>
+      <p className="mt-1 text-sm font-semibold text-red-400">
+        {error?.message}
+      </p>
     </div>
   );
 };
 
 type TextAreaProps = { rows?: number } & FormElement;
 
-const TextArea = ({ label, register, rows = 3 }: TextAreaProps) => {
+const TextArea = ({ label, register, error, rows = 3 }: TextAreaProps) => {
   return (
-    <div className="relative my-3 text-white opacity-70">
+    <div className="relative my-3 text-gray-400">
       <textarea
         placeholder=" "
         rows={rows}
         {...register(label)}
-        className="peer block w-full resize-none appearance-none rounded-md border-2 border-white bg-transparent px-2.5 pb-2.5 pt-4 text-sm focus:border-primary focus:outline-none focus:ring-0"
+        className="peer block w-full resize-none appearance-none rounded-md border-2 border-gray-400 bg-transparent px-2.5 pb-2.5 pt-4 text-sm focus:border-primary focus:outline-none focus:ring-0"
       />
       <label
         htmlFor={label}
-        className="pointer-events-none absolute left-1 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-slate-950 px-2 text-sm duration-300 peer-placeholder-shown:top-6 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:text-primary"
+        className="pointer-events-none absolute left-1 top-2 z-10 origin-[0] -translate-y-4 scale-90 transform bg-slate-950 px-2 text-sm duration-300 peer-placeholder-shown:top-6 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-90 peer-focus:text-fuchsia-400"
       >
         {label}
       </label>
+      <p className="mt-1 text-sm font-semibold text-red-400">
+        {error?.message}
+      </p>
     </div>
   );
 };
--- lib/schema.ts
+++ lib/schema.ts
@@ -2,9 +2,21 @@ import { z } from "zod";
 
 export const contactSchema = z
   .object({
-    Name: z.string().trim().nonempty(),
-    Email: z.string().trim().email().nonempty(),
-    Message: z.string().trim().max(1000).nonempty(),
+    Name: z
+      .string()
+      .trim()
+      .max(150, { message: "⚠️ Name can be at most 150 characters long." })
+      .nonempty({ message: "⚠️ Name must be non-empty." }),
+    Email: z
+      .string()
+      .trim()
+      .email({ message: "⚠️ Email must be valid." })
+      .nonempty({ message: "⚠️ Email must be non-empty." }),
+    Message: z
+      .string()
+      .trim()
+      .max(1000, { message: "⚠️ Message can be at most 1000 characters long." })
+      .nonempty({ message: "⚠️ Message must be non-empty." }),
   })
   .strict()
   .required();
